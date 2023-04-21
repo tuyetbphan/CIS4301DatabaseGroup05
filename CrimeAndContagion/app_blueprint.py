1456e4a19472be572b4decc5a84c887774cb1cbd
tuyetbphan <tphan1@ufl.edu>
2023-04-20 20:44:22 -0400

    query 2

--- CrimeAndContagion/app_blueprint.py
+++ CrimeAndContagion/app_blueprint.py
@@ -70,13 +70,132 @@ def queryone():
 
     return render_template("queryone.html", graphJSON=graphJSON)
 
-
+###############################################################################################################################
 # Query 2:  How has the number of victims between the ages of 18-49 affected by crimes of theft and COVID-19 developed in 2020?
 @app_blueprint.route('/querytwo')
 def querytwo():
-    return render_template("querytwo.html", graphJSON=dy())
+    # create a cursor object
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT 
+        GONGBINGWONG.Crime.Date_,
+        COUNT(*) AS Value,
+        'Theft Victims' AS Category
+    FROM 
+        GONGBINGWONG.Crime
+        JOIN GONGBINGWONG.Victim ON GONGBINGWONG.Crime.Crime_ID = GONGBINGWONG.Victim.Victim_Of
+    WHERE 
+        GONGBINGWONG.Victim.Age BETWEEN 18 AND 49 AND 
+        GONGBINGWONG.Crime.Crime_Code_Description LIKE '%THEFT%' AND 
+        GONGBINGWONG.Crime.Date_ >= TO_DATE('2019-01-01', 'YYYY-MM-DD') AND 
+        GONGBINGWONG.Crime.Date_ <= TO_DATE('2022-12-31', 'YYYY-MM-DD') 
+    GROUP BY 
+        GONGBINGWONG.Crime.Date_
+    
+    UNION ALL
+
+    SELECT 
+        Case_Date,
+        COUNT(*) AS Value,
+        'COVID-19 Patients' AS Category
+    FROM 
+        COVID_19 JOIN Patient ON Case_ID = Infected_Case
+    WHERE 
+        Age_Group = '18 to 49 years' 
+        AND Case_Date >= TO_DATE('2020-01-01', 'YYYY-MM-DD')  
+        AND Case_Date <= TO_DATE('2022-12-31', 'YYYY-MM-DD') 
+    GROUP BY 
+        Case_Date""")
+    
+    # Fetch all the rows and store them in a list
+    result = cursor.fetchall()
+
+    # Create a Pandas DataFrame from the rows
+    df = pd.DataFrame(result, columns=['Date', 'Value', 'Category'])
+
+    # Pivot the DataFrame to create the data for the chart
+    df_pivot = df.pivot(index='Date', columns='Category', values='Value')
+
+    # Fill in missing values using forward-fill
+    df_pivot = df_pivot.ffill(axis=0, inplace=True)
+
+    # Reset the index so that the Date column becomes a regular column
+    df_pivot.reset_index(inplace=True)
+
+    # Create the chart using Plotly Express
+    fig = px.line(df_pivot, x='Date', y=['Theft Victims', 'COVID-19 Patients'])
+
+    # Get the Plotly JSON data and HTML for embedding the chart
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    fig_data = fig.to_html(full_html=False)
+
+    return render_template("querytwo.html", graphJSON=graphJSON)
+
+    # # execute the first query and save the results
+    # query1 = """SELECT GONGBINGWONG.Crime.Date_, COUNT(*) AS num_victims 
+    #         FROM GONGBINGWONG.Crime 
+    #         JOIN GONGBINGWONG.Victim 
+    #         ON GONGBINGWONG.Crime.Crime_ID = GONGBINGWONG.Victim.Victim_Of 
+    #         WHERE GONGBINGWONG.Victim.Age BETWEEN 18 AND 49 
+    #         AND GONGBINGWONG.Crime.Crime_Code_Description LIKE '%THEFT%' 
+    #         AND GONGBINGWONG.Crime.Date_ >= TO_DATE('2019-01-01', 'YYYY-MM-DD') 
+    #         AND GONGBINGWONG.Crime.Date_ <= TO_DATE('2022-12-31', 'YYYY-MM-DD') 
+    #         GROUP BY GONGBINGWONG.Crime.Date_ 
+    #         ORDER BY GONGBINGWONG.Crime.Date_ ASC"""
+    # cursor.execute(query1)
+    # result1 = cursor.fetchall()
+
+    # # execute the second query and save the results
+    # query2 = """SELECT Case_Date, COUNT(*) AS num_patients 
+    #         FROM COVID_19 
+    #         JOIN Patient 
+    #         ON Case_ID = Infected_Case 
+    #         WHERE Age_Group = '18 to 49 years' 
+    #         AND Case_Date >= TO_DATE('2020-01-01', 'YYYY-MM-DD') 
+    #         AND Case_Date <= TO_DATE('2022-12-31', 'YYYY-MM-DD') 
+    #         GROUP BY Case_Date 
+    #         ORDER BY Case_Date ASC"""
+    # cursor.execute(query2)
+    # result2 = cursor.fetchall()
+
+    # # merge the results
+    # merged_results = []
+    # for i in range(len(result1)):
+    #     merged_results.append([result1[i][0], result1[i][1], result2[i][1]])
+
+    # # convert the merged results into a pandas data frame
+    # df = pd.DataFrame(merged_results, columns=["Date", "Theft Victims", "COVID-19 Patients"])
+
+    # # create a pivot table for the data frame
+    # df_pivot = df.pivot_table(values=["Theft Victims", "COVID-19 Patients"], index="Date")
+
+    # # create a melted version of the pivot table
+    # df_melted = df_pivot.reset_index().melt(id_vars=["Date"], value_vars=["Theft Victims", "COVID-19 Patients"], var_name="Victim Type", value_name="Number of Victims/Patients")
+
+    # # create a subplots object with two traces
+    # fig = make_subplots(rows=1, cols=2, subplot_titles=("Number of Theft Victims", "Number of COVID-19 Patients"))
+
+    # # add traces to the subplots object
+    # fig.add_trace(go.Scatter(x=df_pivot.index, y=df_pivot[("Theft Victims")], mode='lines', name='Theft Victims', yaxis='y1'), row=1, col=1)
+    # fig.add_trace(go.Scatter(x=df_pivot.index, y=df_pivot[("COVID-19 Patients")], mode='lines', name='COVID-19 Patients', yaxis='y2'), row=1, col=2)
+
+    # # update the layout of the subplots object
+    # fig.update_layout(title="Number of Theft Victims and COVID-19 Patients",
+    #               xaxis_title="Date",
+    #               yaxis=dict(title="Number of Theft Victims", titlefont=dict(color="#1f77b4"), tickfont=dict(color="#1f77b4")),
+    #               yaxis2=dict(title="Number of COVID-19 Patients", titlefont=dict(color="#1f77b4"), tickfont=dict(color="#1f77b4"), anchor="free", overlaying="y", side="right"))
+
+    # # update the traces of the subplots object
+    # fig.update_traces(hoverinfo='y+x')
+
+    # # convert the subplots object to JSON
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # # render the JSON in a template
+    # return render_template("querytwo.html", graphJSON=graphJSON)
 
 
+##############################################################################################################
 # Query 3: How does a certain area differ from other areas in terms of all crimes committed from 2010 to 2022? 
 @app_blueprint.route('/querythree')
 def querythree():
@@ -108,6 +227,7 @@ def querythree():
     return render_template("querythree.html", graphJSON=graphJSON)
 
 
+#########################################################################################################
 # Query 4: How do the COVID-19 cases relate to the number of indoor and outdoor crimes from 2020 to 2022?
 @app_blueprint.route('/queryfour')
 def queryfour():
@@ -143,8 +263,9 @@ def queryfour():
     return render_template("queryfour.html")
 
 
-# Query 5: What is the ratio of Hispanics to Non-Hispanics who were hospitalized due to COVID-19 and Hispanics to Non-Hispanics 
-# who experienced [insert crime] from 2020 to the present? Can trend patterns be detected? 
+##################################################################################################################
+# Query 5: What is the ratio of Hispanics to Non-Hispanics who were hospitalized due to COVID-19 and Hispanics to 
+# Non-Hispanics who experienced [insert crime] from 2020 to the present? Can trend patterns be detected? 
 @app_blueprint.route('/queryfive')
 def queryfive():
     return render_template("queryfive.html")
