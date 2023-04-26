9d2223552a90aefc841860c53a2995541b48f997
tuyetbphan <tphan1@ufl.edu>
2023-04-26 01:14:15 -0400

    update query 2

--- CrimeAndContagion/app_blueprint.py
+++ CrimeAndContagion/app_blueprint.py
@@ -75,7 +75,7 @@ def queryone():
 # Query 2:  How has the number of victims between the ages of 18-49 affected by crimes of theft and COVID-19 developed in 2020? And the following two years?
 @app_blueprint.route('/querytwo')
 def querytwo():
-    # create a cursor object
+
     cursor = connection.cursor()
     cursor.execute("""
         SELECT 
@@ -94,7 +94,6 @@ def querytwo():
         TRUNC(Date_, 'MM')
     
     UNION ALL
-
     SELECT 
         Case_Date,
         COUNT(*) AS Value,
@@ -118,95 +117,21 @@ def querytwo():
     df_pivot = df_pivot.ffill(axis=0)
     df_pivot.reset_index(inplace=True)
     print(df_pivot)
-    df_melted = pd.melt(df_pivot, id_vars='Date', value_vars=df['Category'].unique(), value_name='Number of Affected People', var_name='Category')
-    fig = px.line(df_melted, x='Date', y='Number of Affected People', color='Category')
-    print(fig.data[0])
+    
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig.add_trace(go.Scatter(x=df_pivot['Date'], y=df_pivot['Theft Victims'], name='Theft Victims'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df_pivot['Date'], y=df_pivot['COVID-19 Patients'], name='COVID-19 Patients'), secondary_y=True)
+
+    fig.update_layout(title='Theft Victims and COVID-19 Patients',
+                      xaxis_title='Date',
+                      yaxis_title='Number of Theft Victims',
+                      yaxis2_title='Number of COVID-19 Patients')
+    
     fig_data = fig.to_html(full_html=False)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template("querytwo.html", graphJSON=graphJSON)
-
-
-    
-    # result = cursor.fetchall()
-
-    # df = pd.DataFrame(result, columns=['Date', 'Value', 'Category'])
-    # df_pivot = df.pivot(index='Date', columns='Category', values='Value')
-    # df_pivot = df_pivot.ffill(axis=0, inplace=True)
-    # if df_pivot is not None:
-    #     df_pivot.reset_index(inplace=True)
-    #     fig = px.line(df_pivot, x='Date', y=['Theft Victims', 'COVID-19 Patients'])
-    #     print(fig.data[0])
-    #     fig_data = fig.to_html(full_html=False)
-    #     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # return render_template("querytwo.html", graphJSON=graphJSON)
-    # Reset the index so that the Date column becomes a regular column
-    # df_pivot.reset_index(inplace=True)
-    # df_pivot = df_pivot.reset_index()
-
-    # if df_pivot is not None:
-    #     df_pivot.reset_index(inplace=True)
-    # fig = px.line(df_pivot, x='Date', y=['Theft Victims', 'COVID-19 Patients'])
-    # print(fig.data[0])
-    # fig_data = fig.to_html(full_html=False)
-    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # return render_template("querytwo.html", graphJSON=graphJSON)
-
-
-    
-
-
-    # # execute the second query and save the results
-    # query2 = """SELECT Case_Date, COUNT(*) AS num_patients 
-    #         FROM COVID_19 
-    #         JOIN Patient 
-    #         ON Case_ID = Infected_Case 
-    #         WHERE Age_Group = '18 to 49 years' 
-    #         AND Case_Date >= TO_DATE('2020-01-01', 'YYYY-MM-DD') 
-    #         AND Case_Date <= TO_DATE('2022-12-31', 'YYYY-MM-DD') 
-    #         GROUP BY Case_Date 
-    #         ORDER BY Case_Date ASC"""
-    # cursor.execute(query2)
-    # result2 = cursor.fetchall()
-
-    # # merge the results
-    # merged_results = []
-    # for i in range(len(result1)):
-    #     merged_results.append([result1[i][0], result1[i][1], result2[i][1]])
-
-    # # convert the merged results into a pandas data frame
-    # df = pd.DataFrame(merged_results, columns=["Date", "Theft Victims", "COVID-19 Patients"])
-
-    # # create a pivot table for the data frame
-    # df_pivot = df.pivot_table(values=["Theft Victims", "COVID-19 Patients"], index="Date")
-
-    # # create a melted version of the pivot table
-    # df_melted = df_pivot.reset_index().melt(id_vars=["Date"], value_vars=["Theft Victims", "COVID-19 Patients"], var_name="Victim Type", value_name="Number of Victims/Patients")
-
-    # # create a subplots object with two traces
-    # fig = make_subplots(rows=1, cols=2, subplot_titles=("Number of Theft Victims", "Number of COVID-19 Patients"))
-
-    # # add traces to the subplots object
-    # fig.add_trace(go.Scatter(x=df_pivot.index, y=df_pivot[("Theft Victims")], mode='lines', name='Theft Victims', yaxis='y1'), row=1, col=1)
-    # fig.add_trace(go.Scatter(x=df_pivot.index, y=df_pivot[("COVID-19 Patients")], mode='lines', name='COVID-19 Patients', yaxis='y2'), row=1, col=2)
-
-    # # update the layout of the subplots object
-    # fig.update_layout(title="Number of Theft Victims and COVID-19 Patients",
-    #               xaxis_title="Date",
-    #               yaxis=dict(title="Number of Theft Victims", titlefont=dict(color="#1f77b4"), tickfont=dict(color="#1f77b4")),
-    #               yaxis2=dict(title="Number of COVID-19 Patients", titlefont=dict(color="#1f77b4"), tickfont=dict(color="#1f77b4"), anchor="free", overlaying="y", side="right"))
-
-    # # update the traces of the subplots object
-    # fig.update_traces(hoverinfo='y+x')
-
-    # # convert the subplots object to JSON
-    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # # render the JSON in a template
-    # return render_template("querytwo.html", graphJSON=graphJSON)
-    
 
 
 ##############################################################################################################
@@ -245,37 +170,7 @@ def querythree():
 # Query 4: How do the COVID-19 cases relate to the number of indoor and outdoor crimes from 2020 to 2022?
 @app_blueprint.route('/queryfour')
 def queryfour():
-    # cursor = connection.cursor()
-
-    # # do something like fetch, insert, etc.
-    # # cursor.execute("SELECT * FROM TPHAN1.COVID_19")
-    # # cursor.execute("SELECT * FROM TPHAN1.PATIENT")
-    # # cursor.execute("SELECT * FROM GONGBINGWONG.CRIME")
-    # # cursor.execute("SELECT * FROM GONGBINGWONG.VICTIM")
-    # # cursor.execute("SELECT CASE_ID FROM TPHAN1.COVID_19 WHERE CASE_DATE = '01-NOV-20' AND WHERE ")
-    # # cursor.execute("SELECT CASE_ID, COUNT(*) FROM TPHAN1.COVID_19 WHERE CURRENT_STATUS = 'PROBABLE CASE'")
-    # # cursor.execute("SELECT * FROM TPHAN1.PATIENT")
-
-    # sqlTxt = "SELECT DISTINCT CRIME_CODE_1 FROM GONGBINGWONG.CRIME"
-    # cursor.execute(sqlTxt)
-
-    # records = cursor.fetchall()
-    # records = list(records)
-
-    # # for record in records:
-    # #     print(record)
-
-    # fig = go.Figure(
-    #     data=[go.Bar(y=records)],
-    #     layout_title_text="A Figure Displayed with fig.show()"
-    # )
-    # fig.show()
-
-
-    # cursor.close()
-    # connection.close() 
-
-    #Not completely finished.
+    
     cursor = connection.cursor()
     cursor.execute("""
 SELECT
@@ -337,37 +232,14 @@ ORDER BY
     return render_template("queryfour.html", graphJSON=graphJSON)
 
 
-
-
-
-
-
 ##################################################################################################################
 # Query 5: What is the ratio of Hispanics to Non-Hispanics who were hospitalized due to COVID-19 and Hispanics to 
 # Non-Hispanics who experienced [insert crime] from 2020 to the present? Can trend patterns be detected? 
 @app_blueprint.route('/queryfive')
 def queryfive():
     cursor = connection.cursor()
-    # cursor.execute("""
-    # select x.year, x.month, x."Hospitalized Hispanic", y."Hospitalized Non-Hispanic" 
-    # from 
-    # (
-    #     select EXTRACT(YEAR FROM case_Date) AS year, EXTRACT(MONTH FROM case_Date) AS month, count(*) as "Hospitalized Hispanic"     
-    #     from tphan1.covid_19 join tphan1.patient on tphan1.covid_19.case_id = tphan1.patient.infected_case 
-    #     where case_Date >= TO_DATE('01-JAN-10', 'DD-MON-YY') AND case_Date <= TO_DATE('27-MAR-23', 'DD-MON-YY') and tphan1.covid_19.hospitalized = 'Yes' and tphan1.patient.ethnicity = 'Hispanic/Latino'
-    #     GROUP BY EXTRACT(YEAR FROM case_Date), EXTRACT(MONTH FROM case_Date)
-    # ) x 
-    # inner join 
-    # (
-    #     select EXTRACT(YEAR FROM case_Date) AS year, EXTRACT(MONTH FROM case_Date) AS month, count(*) as "Hospitalized Non-Hispanic" 
-    #     from tphan1.covid_19 join tphan1.patient on tphan1.covid_19.case_id = tphan1.patient.infected_case 
-    #     where case_Date >= TO_DATE('01-JAN-10', 'DD-MON-YY') AND case_Date <= TO_DATE('27-MAR-23', 'DD-MON-YY') and tphan1.covid_19.hospitalized = 'Yes' and tphan1.patient.ethnicity = 'Non-Hispanic/Latino'
-    #     GROUP BY EXTRACT(YEAR FROM case_Date), EXTRACT(MONTH FROM case_Date)
-    # ) y on x.year = y.year and x.month = y.month
-    # order by x.year, x.month
-    # """)
     cursor.execute("""
-        WITH covid_data AS (
+    WITH covid_data AS (
     SELECT
         EXTRACT(YEAR FROM case_Date) AS year,
         EXTRACT(MONTH FROM case_Date) AS month,
@@ -386,6 +258,7 @@ def queryfive():
     ),
     crime_data AS (
     SELECT
+        crime_code_description,
         EXTRACT(YEAR FROM date_) AS year,
         EXTRACT(MONTH FROM date_) AS month,
         COUNT(CASE WHEN descent = 'H' THEN 1 END) AS "Hispanic Victims",
@@ -409,7 +282,7 @@ def queryfive():
         'DISTURBING THE PEACE'
         )
     GROUP BY 
-        EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_)
+        EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_), crime_code_description
     )
     SELECT
         covid_data.year,
@@ -417,7 +290,8 @@ def queryfive():
         covid_data."Hospitalized Hispanic",
         covid_data."Hospitalized Non-Hispanic",
         crime_data."Hispanic Victims",
-        crime_data."Non-Hispanic Victims"
+        crime_data."Non-Hispanic Victims",
+        crime_data.crime_code_description
     FROM 
         covid_data JOIN crime_data ON 
         covid_data.year = crime_data.year AND covid_data.month = crime_data.month
@@ -425,95 +299,33 @@ def queryfive():
     """)
     query_results = cursor.fetchall()
 
-    # cursor.execute("""
-    # SELECT x.year, x.month, x."Hispanic Victims", y."Non-Hispanic Victims"
-    # FROM
-    # (   
-    #     SELECT  crime_code_description, EXTRACT(YEAR FROM date_) AS year, EXTRACT(MONTH FROM date_) AS month, COUNT(*) AS "Hispanic Victims"
-    #     FROM gongbingwong.victim
-    #     JOIN 
-    #     gongbingwong.crime ON gongbingwong.victim.victim_of = gongbingwong.crime.crime_id
-    #     WHERE descent = 'H'AND
-    #     crime_code_description = 'RESISTING ARREST' OR
-    #     crime_code_description = 'ARSON' OR
-    #     crime_code_description = 'FALSE POLICE REPORT' OR
-    #     crime_code_description = 'CRIMINAL THREATS - NO WEAPON DISPLAYED' OR
-    #     crime_code_description = 'TRESPASSING' OR
-    #     crime_code_description = 'DOCUMENT FORGERY / STOLEN FELONY' OR
-    #     crime_code_description = 'FAILURE TO DISPERSE' OR
-    #     crime_code_description = 'BATTERY POLICE (SIMPLE)' OR
-    #     crime_code_description = 'ASSAULT WITH DEADLY WEAPON ON POLICE OFFICER' OR
-    #     crime_code_description = 'INCITING A RIOT' OR
-    #     crime_code_description = 'DISTURBING THE PEACE' 
-    #     GROUP BY crime_code_description, EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_)
-    # ) x
-    # INNER JOIN
-    # (
-    #     SELECT  crime_code_description, EXTRACT(YEAR FROM date_) AS year, EXTRACT(MONTH FROM date_) AS month, COUNT(*) AS "Non-Hispanic Victims"
-    #     FROM gongbingwong.victim
-    #     JOIN 
-    #     gongbingwong.crime ON gongbingwong.victim.victim_of = gongbingwong.crime.crime_id
-    #     WHERE NOT descent = 'H' AND
-    #     crime_code_description = 'RESISTING ARREST' OR
-    #     crime_code_description = 'ARSON' OR
-    #     crime_code_description = 'FALSE POLICE REPORT' OR
-    #     crime_code_description = 'CRIMINAL THREATS - NO WEAPON DISPLAYED' OR
-    #     crime_code_description = 'TRESPASSING' OR
-    #     crime_code_description = 'DOCUMENT FORGERY / STOLEN FELONY' OR
-    #     crime_code_description = 'FAILURE TO DISPERSE' OR
-    #     crime_code_description = 'BATTERY POLICE (SIMPLE)' OR
-    #     crime_code_description = 'ASSAULT WITH DEADLY WEAPON ON POLICE OFFICER' OR
-    #     crime_code_description = 'INCITING A RIOT' OR
-    #     crime_code_description = 'DISTURBING THE PEACE' 
-    #     GROUP BY crime_code_description, EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_)
-    # ) y ON x.year = y.year and x.month = y.month AND x.crime_code_description=y.crime_code_description
-    # ORDER BY x.crime_code_description, x.year, x.month
-    # """)
-    # query_results2 = cursor.fetchall()
-
-    df = pd.DataFrame(query_results, columns=['Year', 'Month', 'Hospitalized Hispanic', 'Hospitalized Non-Hispanic', 'Hispanic Victims', 'Non-Hispanic Victims'])
+    df = pd.DataFrame(query_results, columns=['Year', 'Month', 'Hospitalized Hispanic', 'Hospitalized Non-Hispanic', 'Hispanic Victims', 'Non-Hispanic Victims', 'Crime Type'])
 
     df['Date'] = pd.to_datetime(df['Year'])
     df['Date'] = pd.to_datetime(df[['Year', 'Month']].assign(day=1))
-    # df_pivot = df.pivot_table(index='Date', values=['HospitalizedHispanic', 'HospitalizedNonHispanic'])
-    # trace0 = go.Scatter(x=df_pivot.index, y=df_pivot.HospitalizedHispanic, mode='lines', name='Hospitalized Hispanics')
-    # trace1 = go.Scatter(x=df_pivot.index, y=df_pivot.HospitalizedNonHispanic, mode='lines', name='Hospitalized Non-Hispanics')
-    # data = [trace0, trace1]
-    # layout = go.Layout(title='Information')
-    # figure = go.Figure(data=data, layout=layout)
-    # df_pivot.ffill(axis=0, inplace=True)
-    # df_pivot.reset_index(inplace=True)
-    # df_melted = pd.melt(df_pivot, id_vars='Date', value_vars=df['Hospitalized'].unique(), value_name='HospitalizedHispanic', var_name='Hospitalized')
+
     fig = px.line(df, x='Date', y=['Hospitalized Hispanic','Hospitalized Non-Hispanic', 'Hispanic Victims', 'Non-Hispanic Victims'], title='Ratio Between Affected Ethnicities')
-    # fig = px.line(df, x='Date', y='Hospitalized Hispanic', title='Total Crimes by Month')
-    # fig.add_scatter(x=df['Date'], y=df['Hospitalized Non-Hispanic'], mode='lines')
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Hospitalized Hispanic'], name='Hospitalized Hispanic'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Hospitalized Non-Hispanic'], name='Hospitalized Non-Hispanic'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Hispanic Victims'], name='Hispanic Victims'), secondary_y=True)
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Non-Hispanic Victims'], name='Non-Hispanic Victims'), secondary_y=True)
+
+    fig.update_layout(
+    title='Ratio Between Affected Ethnicities',
+    yaxis=dict(title='Hospitalized Cases'),
+    yaxis2=dict(title='Number of Crimes', side='right'),
+    legend=dict(x=0, y=1, traceorder="normal", font=dict(family="sans-serif", size=12, color="black"), bgcolor='LightSteelBlue')
+    )
 
     print(fig.data[0])
     fig_data = fig.to_html(full_html=False)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
     return render_template("queryfive.html", graphJSON=graphJSON)
 
-
-    # SELECT x.year, x.month, x.crime_code_description, x."Hispanic Victims", y."Non-Hispanic Victims"
-    # FROM
-    # (
-    #     SELECT  crime_code_description, EXTRACT(YEAR FROM date_) AS year, EXTRACT(MONTH FROM date_) AS month, COUNT(*) AS "Hispanic Victims"
-    #     FROM gongbingwong.victim
-    #     JOIN 
-    #     gongbingwong.crime ON gongbingwong.victim.victim_of = gongbingwong.crime.crime_id
-    #     WHERE descent = 'H'
-    #     GROUP BY crime_code_description, EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_)
-    # ) x
-    # INNER JOIN
-    # (
-    #     SELECT  crime_code_description, EXTRACT(YEAR FROM date_) AS year, EXTRACT(MONTH FROM date_) AS month, COUNT(*) AS "Non-Hispanic Victims"
-    #     FROM gongbingwong.victim
-    #     JOIN 
-    #     gongbingwong.crime ON gongbingwong.victim.victim_of = gongbingwong.crime.crime_id
-    #     WHERE NOT descent = 'H'
-    #     GROUP BY crime_code_description, EXTRACT(YEAR FROM date_), EXTRACT(MONTH FROM date_)
-    # ) y ON x.year = y.year and x.month = y.month AND x.crime_code_description=y.crime_code_description
-    # ORDER BY x.year, x.month
 
 
 
